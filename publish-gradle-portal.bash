b50ddfd4a92064ab9e25ec51d1ec56ef0f9b9104
Joachim Ansorg <github@ja-dev.eu>
2025-03-10 09:52:58 +0100

    New snapshot version

--- publish-gradle-portal.bash
+++ publish-gradle-portal.bash
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if grep -q -E '\-SNAPSHOT' ./gradle.properties 2>&1; then
+  echo >&2 "Cannot publish a SNAPSHOT version to the Gradle Plugin Portal. Terminating."
+  exit 1
+fi
+
 ./gradlew publishPlugins
