46fad9edd446d92cf0a389c4fe30ba5cf3207aaa
Joachim Ansorg <github@ja-dev.eu>
2026-03-06 17:54:50 +0100

    feat: Support bundling the LSP library into a project, support v2 module output, support inlining xi:include LSP snippet references

--- src/main/kotlin/dev/j_a/ide/lsp_gradle_plugin/RelocateLanguageServerPackageTask.kt
+++ src/main/kotlin/dev/j_a/ide/lsp_gradle_plugin/RelocateLanguageServerPackageTask.kt
@@ -1,6 +1,7 @@
 package dev.j_a.ide.lsp_gradle_plugin
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.SetProperty
@@ -11,46 +12,50 @@ import org.gradle.api.tasks.*
  */
 @CacheableTask
 abstract class RelocateLanguageServerPackageTask : ShadowJar() {
+    @get:Input
+    abstract val v2Descriptor: Property<Boolean>
+
     @get:InputFile
     @get:PathSensitive(PathSensitivity.ABSOLUTE)
-    abstract val composedPluginJar: RegularFileProperty
+    abstract val pluginJar: RegularFileProperty
 
     @get:Input
-    abstract val relocateLibraryPackages: Property<Boolean>
-
-    @get:Input
+    @get:Optional
     abstract val packagePrefix: Property<String>
 
     @get:Input
     abstract val enabledLanguageIds: SetProperty<String>
 
-    @get:Input
-    abstract val pluginXmlFiles: SetProperty<String>
+    @get:InputFiles
+    @get:Classpath
+    @get:Optional
+    abstract val lspLibrary: ConfigurableFileCollection
 
     @TaskAction
     override fun copy() {
-        val packagePrefix = packagePrefix.get()
-        if (packagePrefix.isEmpty() || packagePrefix == LSP_PACKAGE_PREFIX) {
-            throw IllegalArgumentException("packagePrefix must be set to a non-empty value different from $LSP_PACKAGE_PREFIX")
-        }
-
         // Change the target package to be inside your own plugin's package
         // For v2 descriptors, it must be inside the package specified by the 'package' attribute of <idea-plugin>
-        if (relocateLibraryPackages.get()) {
+        val packagePrefix = packagePrefix.get()
+        if (packagePrefix.isNotEmpty()) {
+            if (packagePrefix == LSP_PACKAGE_PREFIX) {
+                throw IllegalArgumentException("packagePrefix must be set to a non-empty value different from $LSP_PACKAGE_PREFIX")
+            }
             relocate(LSP_PACKAGE_PREFIX, packagePrefix)
+            relocate(ANTLR4_PACKAGE_PREFIX, "$packagePrefix.$ANTLR4_PACKAGE_PREFIX")
+            // fixme: This break LSP4J own reflection, which is not updated to reference the relocated package
+            //relocate(LSP4J_PACKAGE_PREFIX, "$packagePrefix.$LSP4J_PACKAGE_PREFIX")
         }
 
         val enabledPsiLanguages = enabledLanguageIds.getOrElse(emptySet())
-        transform(UpdatePluginXmlTransformer(enabledPsiLanguages, logger))
+        transform(UpdatePluginXmlTransformer(v2Descriptor.get(), enabledPsiLanguages, logger))
 
-        val pluginXmlFiles = pluginXmlFiles.getOrElse(emptySet())
-        if (pluginXmlFiles.isNotEmpty()) {
-            transform(UpdateNamedPluginXmlTransformer(pluginXmlFiles))
+        from(pluginJar.map { jarFile -> archiveOperations.zipTree(jarFile) })
+        if (!lspLibrary.isEmpty) {
+            logger.info("Bundling LSP library into JAR")
+            from(lspLibrary.map { jar -> archiveOperations.zipTree(jar) })
         }
 
-        from(composedPluginJar.map { jarFile -> archiveOperations.zipTree(jarFile) })
-
-        logger.info("Relocating LSP and DAP libraries in ${composedPluginJar.get()} into package $packagePrefix, PSI languages: $enabledPsiLanguages")
+        logger.info("Relocating LSP and DAP libraries in ${pluginJar.get()} into package $packagePrefix, PSI languages: $enabledPsiLanguages")
         super.copy()
     }
 }
